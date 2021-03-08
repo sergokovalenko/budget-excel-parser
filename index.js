2ff587480403880ad44003c1dd3c7dc437c91331
sergei_kovalenko <sergokovalenko.dev@gmail.com>
2021-03-08 13:03:19 +0400

    basic filtering and mapping

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const xlsx = require('node-xlsx').default;
+const json = require('./categoryMapping.json');
 
 const valuableRows = [11, 14, 1, 9];
 
-const workSheetsFromFile = xlsx.parse(`${__dirname}/operations.xls`);
+const workSheetsFromFile = xlsx.parse(`${__dirname}/operations2.xls`);
 const { data } = workSheetsFromFile[0];
-const newTable = data.map(row => {
-    return valuableRows.map(rowInd => row[rowInd]);
-});
+const newTable = data.map(row => valuableRows.map(rowInd => row[rowInd]))
+    .filter(row => [row.length - 1])
+    .filter(row => !json.useless[row[row.length - 1]]);
 
 console.log(newTable);
