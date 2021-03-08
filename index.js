a702d5fefbbabf6d643fe644c3520a3d074a157e
sergei_kovalenko <sergokovalenko.dev@gmail.com>
2021-03-08 13:10:21 +0400

    Map categories

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const xlsx = require('node-xlsx').default;
 const json = require('./categoryMapping.json');
 
 const valuableRows = [11, 14, 1, 9];
+const categories = Object.entries(json.categories)
+    .reduce((acc, [categoryNumber, values]) => {
+        values.forEach(tinkCategoryName => {
+            acc[tinkCategoryName] = categoryNumber;
+        });
+
+        return acc;
+    }, {});
 
 const workSheetsFromFile = xlsx.parse(`${__dirname}/operations2.xls`);
 const { data } = workSheetsFromFile[0];
 const newTable = data.map(row => valuableRows.map(rowInd => row[rowInd]))
-    .filter(row => [row.length - 1])
-    .filter(row => !json.useless[row[row.length - 1]]);
+    .filter(row => row[row.length - 1])
+    .filter(row => !json.useless[row[row.length - 1]])
+    .map(row => row.map((cell, i) => i === row.length - 1 ? categories[cell] : cell))
 
 console.log(newTable);
