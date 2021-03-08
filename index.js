f5f89a86c268d4524aecb648f7c8798cb8aabd31
sergei_kovalenko <sergokovalenko.dev@gmail.com>
2021-03-08 12:37:37 +0400

    Get excel rows

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello node');
+const xlsx = require('node-xlsx').default;
+
+const workSheetsFromFile = xlsx.parse(`${__dirname}/operations.xls`);
+const { data } = workSheetsFromFile[0];
+
+console.log(data);
