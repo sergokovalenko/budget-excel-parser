b25bf46b2aa76e33a434365072922015778b6c82
sergei_kovalenko <sergokovalenko.dev@gmail.com>
2021-03-08 12:44:19 +0400

    Mapping to my excel format

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const xlsx = require('node-xlsx').default;
 
+const valuableRows = [11, 14, 1, 9];
+
 const workSheetsFromFile = xlsx.parse(`${__dirname}/operations.xls`);
 const { data } = workSheetsFromFile[0];
+const newTable = data.map(row => {
+    return valuableRows.map(rowInd => row[rowInd]);
+});
 
-console.log(data);
+console.log(newTable);
