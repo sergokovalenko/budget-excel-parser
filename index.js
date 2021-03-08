ccbe33216b8b59afc5bd8f996419dfe470259d62
sergei_kovalenko <sergokovalenko.dev@gmail.com>
2021-03-08 13:21:12 +0400

    Saving mapped file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const xlsx = require('node-xlsx').default;
+const fs = require('fs');
+
 const json = require('./categoryMapping.json');
 
 const valuableRows = [11, 14, 1, 9];
@@ -11,11 +13,20 @@ const categories = Object.entries(json.categories)
         return acc;
     }, {});
 
-const workSheetsFromFile = xlsx.parse(`${__dirname}/operations2.xls`);
+const workSheetsFromFile = xlsx.parse(`${__dirname}/operations.xls`);
 const { data } = workSheetsFromFile[0];
 const newTable = data.map(row => valuableRows.map(rowInd => row[rowInd]))
     .filter(row => row[row.length - 1])
     .filter(row => !json.useless[row[row.length - 1]])
     .map(row => row.map((cell, i) => i === row.length - 1 ? categories[cell] : cell))
 
-console.log(newTable);
+const bin = xlsx.build([{ name: '', data: newTable }]);
+
+fs.writeFile('result.xls', bin,  'binary', (err) => {
+    if (err) {
+        console.log(err);
+        console.log('The file was not saved!');
+    } else {
+        console.log("The file was saved!");
+    }
+});
